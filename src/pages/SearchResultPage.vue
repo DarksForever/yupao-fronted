fe30c0bd732d4928c394596a966a72fce1784129
lcy <liaochangyi@xgd.com>
2023-12-22 14:46:42 +0800

    完成用户查询和用户更新的前后端对接

--- src/pages/SearchResultPage.vue
+++ src/pages/SearchResultPage.vue
@@ -1,6 +1,26 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-import { useRoute } from 'vue-router';
+<template>
+    <van-card v-for="user in userList" :desc="user.profile" :title="`${user.username} (${user.planetCode})`"
+        :thumb="user.avatarUrl">
+        <template #tags>
+            <van-tag plain type="danger" v-for="tag in tags" style="margin-right: 8px; margin-top: 8px">
+                {{ tag }}
+            </van-tag>
+        </template>
+        <template #footer>
+            <van-button size="mini">联系我</van-button>
+        </template>
+    </van-card>
+    <van-empty v-if="!userList || userList.length < 1" description="搜索结果为空" />
+</template>
+  
+<script setup >
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import { Toast } from "vant";
+
+import myAxios from "../plugins/myAxios.js";
+
+import qs from 'qs'
 
 const route = useRoute();
 const { tags } = route.query;
@@ -19,21 +39,44 @@ const mockUser = ref({
     createTime: new Date(),
 })
 
-const userList = ref({ mockUser })
+const userList = ref([]);
+
+
+onMounted(async () => {
+    // 为给定 ID 的 user 创建请求
+    const userListData = await myAxios.get('/user/search/tags', {
+        withCredentials: false,
+        params: {
+            tagNameList: tags
+        },
+
+
+        //序列化
+        paramsSerializer: {
+            serialize: params => qs.stringify(params, { indices: false }),
+        }
+    })
+        .then(function (response) {
+            console.log('/user/search/tags succeed', response);
+            Toast.success('请求成功');
+            return response.data?.data;
+        })
+        .catch(function (error) {
+            console.log('/user/search/tags error', error);
+            Toast.fail('请求失败');
+        });
+    if (userListData) {
+        userListData.forEach(user => {
+            if (user.tags) {
+                user.tags = JSON.parse(user.tags);
+            }
+        })
+        userList.value = userListData;
+    }
+})
+
+
+
 </script>
-
-<template>
-    <van-card v-for="user in userList" :desc="user.profile" :title="`${user.username} (${user.planetCode})`"
-        :thumb="user.avatarUrl">
-        <template #tags>
-            <van-tag plain type="danger" v-for="tag in tags" style="margin-right: 8px; margin-top: 8px">
-                {{ tag }}
-            </van-tag>
-        </template>
-        <template #footer>
-            <van-button size="mini">联系我</van-button>
-        </template>
-    </van-card>
-</template>
-
+  
 <style scoped></style>
--- src/pages/UserEditPage.vue
+++ src/pages/UserEditPage.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { useRoute } from "vue-router";
+import myAxios from "../plugins/myAxios.js";
 
-const route = new useRoute();
+const route = useRoute();
 //获取修改前的值
 const editUser = ref({
     editKey: route.query.editKey,
@@ -10,9 +11,18 @@ const editUser = ref({
     currentValue: route.query.currentValue,
 })
 //提交修改
-const onSubmit = (value) => {
-    //todo
-    console.log('onSubmit:' + value);
+const onSubmit = async () => {
+    const res = await myAxios.post('/user/update', {
+        'id': 1,
+        [editUser.value.editKey]: editUser.value.currentValue
+    })
+    console.log(res, '更新请求')
+    if (res.code === 0 && res.data > 0) {
+        route.back();
+        console.log('更新成功')
+    } else {
+        console.log('更新失败')
+    }
 }
 </script>
 
